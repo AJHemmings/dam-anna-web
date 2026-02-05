163200f77ecfa45abbd29ed2ec11bb0f279c2226
Adam Hemmings <ajhemmings@users.noreply.github.com>
2026-02-05 14:50:52 +0000

    refactor: replacing arbitrary value (mb-[350px]) with a standardized theme scale value (mb-87.5)

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
   return (
-    <header className="col-start-2 col-span-5 text-[2.5rem] p-8 mb-[350px]">
+    <header className="col-start-2 col-span-5 text-[2.5rem] p-8 mb-87.5">
       <img src="/logo3-resize.png" alt="Dam Anna Logo" />
     </header>
   );
