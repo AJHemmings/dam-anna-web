5fe1619236684e1f888c9d2480086958ffa04eb7
Adam Hemmings <ajhemmings@users.noreply.github.com>
2026-02-10 11:55:12 +0000

    feat: add smooth scroll to gigs section on gigs navigation button

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Container from './components/Container';
 import GigPhotosSection from './components/sections/GigPhotoSection';
 import GallerySection from './components/sections/GallerySection';
 import Navigation from './components/Navigation';
+import { scrollToSection, scrollToTop } from './utils/ScrollToSection';
 // import VideoSection from './components/sections/VideoSection';
 
 export default function App() {
@@ -20,6 +21,16 @@ export default function App() {
 
   function handleNavClick(itemId) {
     console.log(`Navigation item clicked: itemId`);
+    switch (itemId) {
+      case 'home':
+        scrollToTop();
+        break;
+      case 'gigs':
+        scrollToSection('gigs-section');
+        break;
+      default:
+        console.log('handler for ${itemID} not implemented yet');
+    }
     // TODO: Will handle modal and scrolling in future phase after we add more content and sections
   }
 
@@ -53,7 +64,7 @@ export default function App() {
       >
         <HeroSection />
         <BlockQuote>We make music.</BlockQuote>
-        <div className="col-start-2 col-span-10 mb-87.5 flex gap-4 items-start">
+        <div id='gigs-section' className="col-start-2 col-span-10 mb-87.5 flex gap-4 items-start">
         <GigsSection />
         <GigPhotosSection />
         </div>
